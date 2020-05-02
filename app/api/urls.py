299b0e4d38dfd273572ea4baf2086a6369f9d5f2
FurkanOzkaya <48178781+FurkanOzkaya@users.noreply.github.com>
2020-05-02 22:20:57 +0300

    chnangin names of urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -5,8 +5,8 @@ from app.api.views import ParkApi
 from app.api.views import ParkApiList
 
 urlpatterns = [
-    path('get_spesific_park_area/<pk>', ParkApi.as_view()),
-    path('add_park_area', ParkApi.as_view()),
+    path('get_spesific_park_areas/<pk>', ParkApi.as_view()),
+    path('add_park_areas', ParkApi.as_view()),
     path('display_close_areas', ParkApiList.as_view()),
     path('delete_areas/<pk>', ParkApi.as_view()),
 ]
